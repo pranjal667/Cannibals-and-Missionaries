28a9b7203eba96e6c98349cc89604f65df1edbfd
Pranjal Ghimire <68840305+pranjal667@users.noreply.github.com>
2022-08-19 21:54:03 +0545

    Add files via upload

---
+++ PranjalGhimire15Cannibal&MissionariesAssignment/ReadMe.txt
@@ -0,0 +1,6 @@
+Before Compiling please confirm you have following modules installed:
+
+1. numpy
+2. pydot
+3. graphviz (https://graphviz.gitlab.io/download/)
+
---
+++ PranjalGhimire15Cannibal&MissionariesAssignment/main.py
@@ -0,0 +1,13 @@
+from time import time
+from BFS import bfs
+from node import Node
+
+initial_state = [3, 3, 1]
+
+Node.num_of_instances = 0
+t0 = time()
+solution = bfs(initial_state)
+t1 = time()-t0
+print('Solution:', solution)
+print('space:', Node.num_of_instances)
+print('time:', t1, 'seconds')
